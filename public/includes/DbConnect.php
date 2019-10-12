c01d4d6dcf8ad5afac4b0127ffde74b07ae8aadc
abhigupta19 <gabhi8740@gmail.com>
2019-10-12 12:20:31 +0530

    first

---
+++ public/includes/DbConnect.php
@@ -0,0 +1,15 @@
+<?php
+class Dbconnect{
+    private $con;
+    function connect()
+    {
+        include_once dirname(__FILE__).'/Constant.php';
+        $this->con=new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+        if(mysqli_connect_errno())
+        {
+            echo "failed to connect".mysqli_connect_error();
+            return null;
+        }
+        return $this->con;
+    }
+}
